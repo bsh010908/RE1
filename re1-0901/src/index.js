124fc321d17d29ab2393a442a275e0295c794890
bsh010908 <bsh010908@gmail.com>
2025-09-01 10:22:43 +0900

    sdf

--- re1-0901/src/index.js
+++ re1-0901/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import App from "./App2";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+root.render(<App2 />);
+
